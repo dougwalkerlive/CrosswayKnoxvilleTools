ad742945d3f93996eb01470cee12fbcc0c973789
DanielY1783 <daniel.yan@vanderbilt.edu>
2020-06-24 11:33:20 -0400

    json to csv code

--- UpdateSermonMP3s/ReadJsonSermons.py
+++ UpdateSermonMP3s/ReadJsonSermons.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python
-import os
 import sys
 import json
+import pandas as pd
 
 # Get the filename from the commandline argument
-if len(sys.argv) < 1:
+if len(sys.argv) < 2:
     print('Need to input a .json filename as an argument')
     exit()
 else:
     filename = sys.argv[1]
 
 # load the data from the json file
-with open(filename) as file:
-    sermons = json.load(file)
+sermons_df= pd.read_json(filename)
 
-# print the title of each sermon
-for s in sermons:
-    print(s['Title'] + ' by ' + s['Speaker'])
-
+# convert to csv
+sermons_df.to_csv("ExtractSermons.csv", index=False)
