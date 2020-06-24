be696088538c497fef5cffb209b805d6eb505b79
DanielY1783 <daniel.yan@vanderbilt.edu>
2020-06-24 14:09:01 -0400

    Added series index to csv file

--- UpdateSermonMP3s/ReadJsonSermons.py
+++ UpdateSermonMP3s/ReadJsonSermons.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 import sys
-import json
 import pandas as pd
 
-# Get the filename from the commandline argument
-if len(sys.argv) < 2:
-    print('Need to input a .json filename as an argument')
-    exit()
-else:
-    filename = sys.argv[1]
+def main():
+    # Get the filename from the commandline argument
+    if len(sys.argv) < 2:
+        print('Need to input a .json filename as an argument')
+        exit()
+    else:
+        filename = sys.argv[1]
 
-# load the data from the json file
-sermons_df= pd.read_json(filename)
+    # load the data from the json file as pandas dataframe
+    df = pd.read_json(filename)
 
-# convert to csv
-sermons_df.to_csv("ExtractSermons.csv", index=False)
+    # Drop the descriptions column since it is always empty.
+    df = df.drop("Description", axis=1)
+
+    # Add column with the index number within each series
+    df["SeriesIndex"] = df.groupby("SeriesName")["Date"].rank(method="first", ascending=True)
+    df.SeriesIndex = df.SeriesIndex.astype(int)
+    
+    # convert to csv
+    df.to_csv("ExtractSermons.csv", index=False)
+
+if __name__ == '__main__':
+    main()
